f20de3ea221a8c959bf81269b40ddd1493f90a47
Amanda Cantiliano Idalgo <amandalavigne2@gmail.com>
2023-10-04 22:01:34 -0300

    Add files via upload

--- cadastro_vendas/src/main/java/controllers/inserirVendaPagDigitalServlet.java
+++ cadastro_vendas/src/main/java/controllers/inserirVendaPagDigitalServlet.java
@@ -3,6 +3,7 @@ package controllers;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,22 +19,42 @@ public class inserirVendaPagDigitalServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		String item = request.getParameter("item");
-		String valorTxt = request.getParameter("valor");
+		String nomeCliente = request.getParameter("nomeCliente");
+		String emailCliente = request.getParameter("emailCliente");
+		String nomeProduto = request.getParameter("nomeProduto");
+		double valorProduto = Float.parseFloat(request.getParameter("valorProduto"));
+		String formaPagamento = request.getParameter("formaPagamento");
+		String tipoDeEntrega = request.getParameter("tipoDeEntrega");
+		
+		//double valor = Double.parseDouble(valorTxt);
 		
 		VendaPagDigital v = new VendaPagDigital();
-		v.setProduto(item);
-		v.setValorPago(serialVersionUID);
+		v.setCliente(nomeCliente);
+		v.setClienteEmail(emailCliente);
+		v.setProduto(nomeProduto);
+		v.setTipoEntrega(tipoDeEntrega);
+		v.setTipoPagamento(formaPagamento);
+		v.setValorPago(valorProduto);
 		
+		VendaPagDigitalDAO vendaDAO = new VendaPagDigitalDAO(); 
 		
+		int status = vendaDAO.inserir(v);
+		
+		request.setAttribute("venda", v);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("ListaDeVendas.jsp");
 		PrintWriter htmlSaida = response.getWriter();
 		
+		if(formaPagamento.equals("cartao")) {
+			v.setStatusPagamento("Pagamento Confirmado");
+			
+		}
+		else {
+			v.setStatusPagamento("Aguardando Pagamento");
+		}
 		
-		htmlSaida.print("<p style='color: red'>Venda inserida com sucesso!</p>"); 
-		htmlSaida.print("<p>Nome:</p>"+v.getCliente()); 
-		
-	}
 
+	}
 }
